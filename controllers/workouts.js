53c41799bea67fcf9f8855d208d1486651e3b172
mdbow22 <mdbow22@gmail.com>
2021-12-05 21:53:55 -0500

    finished route for lastworkout for index page

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const db = require('../models');
 
+//get all workouts
+router.get('/workouts', async (req, res) => {
+    const workouts = await db.Workout.aggregate([
+        {$addFields: {totalDuration: {$sum: '$exercises.duration'}}}
+    ]);
+
+    res.json(workouts);
+});
+
+//get last 7 workouts and aggregate exercise duration
 router.get('/workouts/range', async (req, res) => {
     const latest = await db.Workout.aggregate([
         {$sort: {day: -1}},
--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/stats.html'));
 });
 
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/exercise.html'));
+});
+
 //make server go
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
